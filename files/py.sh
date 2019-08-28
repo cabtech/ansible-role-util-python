5e97f45baa715a2fb32d596e50a29d2a5e550522
Tim Short <tim.short@cabernet.tech>
2019-08-28 07:13:28 +0100

    migration

---
+++ files/py.sh
@@ -0,0 +1,16 @@
+# --------------------------------
+# ansible managed bash include file
+# --------------------------------
+
+alias f16='python2 -m flake8 --ignore=E501'
+alias f24='python3 -m flake8 --ignore=E501'
+alias py2="python2"
+alias py3="python3"
+alias  pl="pylint"
+alias pl3="pylint3"
+
+alias pytags='ctags --languages=python --recurse=yes .'
+
+alias pipuppip='pip install --upgrade pip'
+
+# --------------------------------
